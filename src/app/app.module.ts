e414602dca4fee3ee86559ab6d7fe7ac76f99967
Andre <andre.fillipe20@gmail.com>
2021-01-04 23:44:01 -0300

    integracao com api backend no Heroku funcionando, rota no Angular /clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ClientesComponent, MessagesComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/cliente.ts
@@ -0,0 +1,9 @@
+export interface Cliente {
+  id: number;
+  nome: string;
+  cpf : string;
+  email: string;
+  sexo: string;
+  telefone: string;
+  dataNascimento: Date;
+}
